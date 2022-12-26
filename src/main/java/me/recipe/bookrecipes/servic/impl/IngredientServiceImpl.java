c1c32711f16ace526c6af5978d1ae814e9a99ba6
Света <svetik24082@mail.ru>
2022-12-26 22:50:01 +0500

    Доработала твои замечания. Но у меня не получается вывести  шаги - "preparingSteps". Что не так?

---
+++ src/main/java/me/recipe/bookrecipes/servic/impl/IngredientServiceImpl.java
@@ -0,0 +1,39 @@
+package me.recipe.bookrecipes.servic.impl;
+
+import me.recipe.bookrecipes.model.Ingredient;
+import me.recipe.bookrecipes.servic.IngredientService;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+
+    @Service
+    public class IngredientServiceImpl implements IngredientService {
+        private final Map<Integer, Ingredient> ingredients = new HashMap<>();
+        private int id = 0;
+
+        public Collection<Ingredient> getAllIngredient() {// получение всех ингред
+            return ingredients.values();
+        }
+
+        public Ingredient addIngredient(Ingredient ingredient) {  // доб рец
+            if (ingredients.containsKey(id)) {
+                throw new RuntimeException("Такой ингредиент уже есть");
+            } else {
+                ingredients.put(id++, ingredient);
+            }
+            return ingredient;
+        }
+
+
+        public Ingredient getIngredientById(int id) {
+            if (ingredients.containsKey(id)) {
+                return ingredients.get(id);
+            } else {
+                throw new RuntimeException(" Такого ингредиента нет!");
+            }
+        }
+    }
+
